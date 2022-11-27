9d7ea65224f0d0ef39865e351481c2c3867f5e2d
Diego Carvajal <dicarvajalb@unal.edu.co>
2022-11-27 08:02:53 -0500

    Cambios minimos

--- public/semantic/Listener.js
+++ public/semantic/Listener.js
@@ -11,6 +11,19 @@ export default class Listener extends JavaScriptParserListener {
     enterVarModifier(ctx){
         //this.AllComp = <this.AllComp><p>Estuve en una var modifier</p></this.AllComp>
         //console.log(<this.AllComp/>)
+        console.log("keyword", ctx.let_())
+    }
+    enterLet_(ctx){
+        console.log("let_", ctx.NonStrictLet().value)
+    }
+    visitTerminal(node){
+        if (node.getText == "let"){
+            this.AllComp.append("<BlueLetComp>let</BlueLetComp>")
+        }
+        
+    }
+    enterKeyword(ctx){
+        console.log("keyword", ctx)
     }
     
 }
